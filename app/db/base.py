c8b7330615965f78cf731485aaa99a59b2840dcc
Dopester03 <soumiksutradhar0612@gmail.com>
2025-10-19 13:28:42 +0530

    Created a base class file(base_class.py) and updated base.py

---
+++ app/db/base.py
@@ -0,0 +1,1 @@
+from app.db.base_class.py import Base # models present here will be dete ted by Alembic automatically
---
+++ app/db/base_class.py
@@ -0,0 +1,3 @@
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base() # definition of SQLAlchemy base class from which all models will inherit
